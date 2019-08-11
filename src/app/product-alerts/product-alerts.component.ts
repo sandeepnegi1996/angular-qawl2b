340ff2deb8321d3442591a93405f709efa6a0257
sandeepnegi1996 <9041128179sandy@gmail.com>
2019-08-11 19:35:31 +0530

    added a notify me button

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Input } from '@angular/core';
 @Component({
   selector: 'app-product-alerts',
   templateUrl: './product-alerts.component.html',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductAlertsComponent implements OnInit {
 
+@Input() product;
   constructor() { }
 
   ngOnInit() {
